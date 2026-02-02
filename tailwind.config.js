ac3e0493af61f503e0a796c6c5090c946ae4c957
LowkeyLuxuryInc <jakely36@gmail.com>
2026-02-02 11:41:46 -0600

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: [
+    "./src/**/*.{html,js,jsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        gold: '#D4AF37',
+        black: '#1a1a1a',
+      },
+    },
+  },
+  plugins: [],
+};
